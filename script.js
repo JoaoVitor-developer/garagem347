1e5be7ede287e6b287e68d4a813d4fa22284a400
JoaoVitor-developer <joaovitorleonardodias@gmail.com>
2025-05-16 12:14:39 -0300

    novo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const services = document.querySelectorAll('.service');
+ const services = document.querySelectorAll('.service');
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -8,11 +8,11 @@ const services = document.querySelectorAll('.service');
       });
     }, {
       threshold: 0.1
-});
+    });
 
     services.forEach(service => {
       service.style.opacity = '0';
       service.style.transform = 'translateY(40px)';
       service.style.transition = 'all 0.6s ease-out';
       observer.observe(service);
-});
+    });
